d7a78978eed58ec359a1e5d0ccc52feb4cf28284
Davis Louis <Louis.Davis@HCAHealthcare.com>
2016-10-07 08:58:30 -0500

    Added the developer test

---
+++ Part1.General.UnitTests/RepositoryUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Part1.General.UnitTests
+{
+    [TestClass]
+    public class RepositoryUnitTests
+    {
+        [TestMethod]
+        public void TestSearchMethod()
+        {
+        }
+    }
+}
---
+++ Part1.General/PatientRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1.General
+{
+    public class PatientRepository
+    {
+        private List<Patient> Data = new List<Patient>();
+
+        // Implement Search here
+    }
+}
